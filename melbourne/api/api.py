bb3781d565258e43feca6780351ae7e48d617d82
Aditya Duri <aditya.duri@gmail.com>
2024-11-01 22:48:04 +0000

    Removing unused class

--- melbourne/api/api.py
+++ melbourne/api/api.py
@@ -1,19 +1,12 @@
 import logging
 
 from fastapi import FastAPI, Form, UploadFile
-from pydantic import BaseModel
 
 from melbourne.contest.contest import Contest
-from melbourne.contest.entry import Entry
 
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
-
-
-class ParseContestResponse(BaseModel):
-    entries: list[Entry]
-    voters: list[str]
 
 
 @app.post("/api/contest")
