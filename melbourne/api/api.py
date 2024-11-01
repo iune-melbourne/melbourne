0081cbbe90081eaf6ce0c7c5a90ea8592f605109
Aditya Duri <aditya.duri@gmail.com>
2024-11-01 22:53:55 +0000

    Adding support for settings file

--- melbourne/api/api.py
+++ melbourne/api/api.py
@@ -3,10 +3,19 @@ import logging
 from fastapi import FastAPI, Form, UploadFile
 
 from melbourne.contest.contest import Contest
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+
+
+class Settings(BaseSettings):
+    working_dir: str
+    fonts_dir: str
+    flags_dir: str
+
+    model_config = SettingsConfigDict(env_file=".env")
 
 
 @app.post("/api/contest")
